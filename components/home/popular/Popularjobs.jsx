3e65d7a1cd1725f5c76173eda24ccd0c210a3731
mohammad <mohammadnv79@gmail.com>
2023-07-30 07:52:55 +0330

    add popular job card component

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,46 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 
-import styles from './popularjobs.style'
+
+import styles from "./popularjobs.style";
+import { COLORS, SIZES } from "../../../constants";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
 
 const Popularjobs = () => {
-  return (
-    <View>
-      <Text>Popularjobs</Text>
-    </View>
-  )
-}
+  const isLoading = false;
+  const error = false;
 
-export default Popularjobs
+  return (
+    <View className={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show more</Text>
+        </TouchableOpacity>
+      </View>
+      <View>
+        {isLoading ? (
+          <ActivityIndicator size={"large"} color={COLORS.primary} />
+        ) : error ? (
+          <Text>Somting went wrong</Text>
+        ) : (
+          <FlatList
+            data={[1, 2, 3, 4, 5]}
+            renderItem={({ item }) => <PopularJobCard />}
+            keyExtractor={item => item}
+            horizontal
+            contentContainerStyle={{columnGap: SIZES.medium}}
+          />
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Popularjobs;
